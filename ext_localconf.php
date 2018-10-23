3078c21730842afefc5d774459c1a986b7a25b41
Sebastian Klein <sebastian@sklein-medien.de>
2018-10-23 20:55:43 +0200

    [TASK] Use new import syntax for TSConfig file

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,7 +4,7 @@ if (!defined('TYPO3_MODE')) {
 }
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TSconfig/User.tsconfig">'
+    '@import "EXT:basetemplate9/Configuration/TSconfig/User.tsconfig"'
 );
 
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['basetemplate9_rte'] = 'EXT:basetemplate9/Configuration/RTE/Custom.yaml';
